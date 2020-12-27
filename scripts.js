e4cb6606b57a7f77738fc4f33dbd70e6c2c00cf5
pedrosouza423 <pedro.souza4237@gmail.com>
2020-12-27 20:18:43 -0300

    Modal da página inicial e da página de receitas terminado

--- scripts.js
+++ scripts.js
@@ -3,11 +3,18 @@ const modalOverlay = document.querySelector('.modal-overlay')
 
 for(let card of cards){
     card.addEventListener("click", function(){
-        const idFoto = card.getAttribute('id')
-        modalOverlay.querySelector('img').src = `${idFoto}`
+        const idFoto = card.getAttribute('idFoto')
+        const foodName = card.getAttribute('name')
+        const foodBy = card.getAttribute('by')
 
-        const prato = card.querySelector('.card-content p')
-        modalOverlay.querySelector('.modal-info h2').innerHTML = `${prato}`
+        modalOverlay.querySelector('img').src = `${idFoto}`
+        
+
+        modalOverlay.querySelector('.modal-info h2').textContent = `${foodName}`
+        modalOverlay.querySelector('.modal-info p').textContent = `${foodBy}`
+
+
+
 
         
 

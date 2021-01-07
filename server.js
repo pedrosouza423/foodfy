874dfa5d79ad5c9ea083ee3e719dd247db73a81a
pedrosouza423 <pedro.souza4237@gmail.com>
2021-01-07 12:45:23 -0300

    Foi criado um servidor express, criado um layout e transferido o html em njk

--- server.js
+++ server.js
@@ -1,6 +1,32 @@
 const express = require('express')
+const nunjucks = require('nunjucks')
+
 const server = express()
 
+//Utilizando parte estática da página
+server.use(express.static('public'))
+
+//Setando view engine
+server.set("view engine","njk")
+
+nunjucks.configure("views",{
+    express:server,
+    autoescape:false,
+    noCache: true
+})
+
+//Rotas do servidor
+server.get("/", function(req,res){
+    return res.render('index')
+})
+
+server.get("/receitas", function(req,res){
+    return res.render('receitas')
+})
+
+server.get("/sobre", function(req,res){
+    return res.render('sobre')
+})
 
 server.listen(5000, function(){
     console.log('server is running')

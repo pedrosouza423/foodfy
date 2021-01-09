c44366f40803b35e26353d9faac7271cc0a83726
pedrosouza423 <pedro.souza4237@gmail.com>
2021-01-09 02:33:00 -0300

    Foi adicionado uma página para cada receita, e foram feitos dados dinamicos do back-end para o front-end

--- server.js
+++ server.js
@@ -19,24 +19,41 @@ nunjucks.configure("views",{
 })
 
 //Rotas do servidor
+//Página HOME
 server.get("/", function(req,res){
-    return res.render('index', {receitas} )
+    const home = {
+        text: 'Aprenda a construir os melhores pratos com receitas criadas por profissionais do mundo inteiro.',
+        best_recips: 'As melhores receitas',
+        most_accessed: 'Mais acessadas'
+    }
+    return res.render('index', {receitas,home} )
 })
 
+//Página de receitas
 server.get("/receitas", function(req,res){
     return res.render('receitas', {receitas})
 })
 
+//Página sobre
 server.get("/sobre", function(req,res){
-    return res.render('sobre')
+    const about = {
+        about: 'Sobre o Foodfy',
+        text1: 'Suspendisse placerat neque neque. Morbi dictum nulla non sapien rhoncus, et mattis erat commodo. Aliquam vel lacus a justo mollis luctus. Proin vel auctor eros, sed eleifend nunc. Curabitur eget tincidunt risus. Mauris malesuada facilisis magna, vitae volutpat sapien tristique eu. Morbi metus nunc, interdum in erat placerat, aliquam iaculis massa. Duis vulputate varius justo pharetra maximus. In vehicula enim nec nibh porta tincidunt. Vestibulum at ultrices turpis, non dictum metus. Vivamus ligula ex, semper vitae eros ut, euismod convallis augue.',
+        text2: 'Fusce nec pulvinar nunc. Duis porttitor tincidunt accumsan. Quisque pulvinar mollis ipsum ut accumsan. Proin ligula lectus, rutrum vel nisl quis, efficitur porttitor nisl. Morbi ut accumsan felis, eu ultrices lacus. Integer in tincidunt arcu, et posuere ligula. Morbi cursus facilisis feugiat. Praesent euismod nec nisl at accumsan. Donec libero neque, vulputate semper orci et, malesuada sodales eros. Nunc ut nulla faucibus enim ultricies euismod.',
+        beginning:'Como tudo começou…',
+        recips:'Nossas receitas'
+    }
+
+    return res.render('sobre', {about})
 })
 
 /* Página individual de cada receita */
-server.get("/receitas/:id", function(req,res){
-    const id = req.params.id
-
-    return res.send(id)
-
+server.get("/receitas/:index", function (req, res) {
+    const receitasIndex = req.params.index
+  
+    const receita = receitas[receitasIndex]
+    // return res.send(receitas[receitasIndex])
+    return res.render('receita',{receita})
 })
 
 server.listen(5000, function(){
